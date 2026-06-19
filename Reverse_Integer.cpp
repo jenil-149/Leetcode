0826c26fb2bf1ceef224c43f88e9fbaacb1325a0
Jenil Mistry <jenil140509@gmail.com>
2026-06-19 23:51:11 +0530

    Update Reverse_Integer solution

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -11,30 +11,27 @@ using namespace std;
             if (result > INT_MAX / 10 || (result == INT_MAX / 10 && pop > 7)) {
                 return 0;
             }
-            // Check for positive overflow before pushing the digit
             
             x /= 10;
-            // Pop the last digit off
             int pop = x % 10;
         
         while (x != 0) {
-    int reverse(int x) {
-        int result = 0;
-public:
-class Solution {
-
-#include <climits>
             
-            // Check for negative overflow (underflow) before pushing the digit
             if (result < INT_MIN / 10 || (result == INT_MIN / 10 && pop < -8)) {
                 return 0;
             }
-            
-            // Push the digit onto the result
             result = result * 10 + pop;
         }
         
         return result;
+        int result = 0;
+    int reverse(int x) {
+public:
+class Solution {
+
+#include <climits>
+    }
+};
 
 int main() {
     // Test cases can be written here
