ac57dea30665f20c2c7950791bb588face4796ed
Jenil Mistry <jenil140509@gmail.com>
2026-06-19 23:48:12 +0530

    Add Reverse_Integer solution

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,31 @@
+/**
+ * LeetCode Problem: Reverse Integer
+ * Auto-synced by LeetCommit
+ * Date: 2026-06-19
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && pop > 7)) {
+                return 0;
+            }
+            // Check for positive overflow before pushing the digit
+            
+            x /= 10;
+            // Pop the last digit off
+            int pop = x % 10;
+        
+        while (x != 0) {
+    int reverse(int x) {
+        int result = 0;
+public:
+
+class Solution {
+#include <climits>
+
+int main() {
+    // Test cases can be written here
+    return 0;
+}
