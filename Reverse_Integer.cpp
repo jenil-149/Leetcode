322f33fe4fd0bf2368095c440aca36f41acf3e3d
Jenil Mistry <jenil140509@gmail.com>
2026-06-19 23:49:18 +0530

    Update Reverse_Integer solution

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -21,9 +21,20 @@ using namespace std;
     int reverse(int x) {
         int result = 0;
 public:
-
 class Solution {
+
 #include <climits>
+            
+            // Check for negative overflow (underflow) before pushing the digit
+            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            // Push the digit onto the result
+            result = result * 10 + pop;
+        }
+        
+        return result;
 
 int main() {
     // Test cases can be written here
