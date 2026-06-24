4d33d57e9abb37459152ca97f0b6ed7d46d88159
Jenil Mistry <jenil140509@gmail.com>
2026-06-24 19:56:45 +0530

    Add Plus_One solution

---
+++ Plus_One.cpp
@@ -0,0 +1,35 @@
+/**
+ * LeetCode Problem: Plus One
+ * Pushed by LeetCommit
+ * Date: 2026-06-24
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& v) {
+        int n = v.size();
+        for(int i = n-1; i >= 0; i--){
+            if(i == n-1)
+                v[i]++;
+            if(v[i] == 10){
+                v[i] = 0;
+                if(i != 0){
+                    v[i-1]++;
+                }
+                else{
+                    v.push_back(0);
+                    v[i] = 1;
+                }
+            }
+        }
+        return v;
+    }
+};
+
+int main() {
+    return 0;
+}
