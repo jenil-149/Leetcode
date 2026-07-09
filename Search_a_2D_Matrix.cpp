73ceadc8628ea73258239d500ecce54af95b6608
Jenil Mistry <jenil140509@gmail.com>
2026-07-09 23:57:54 +0530

    Update Search_a_2D_Matrix solution

--- Search_a_2D_Matrix.cpp
+++ Search_a_2D_Matrix.cpp
@@ -8,23 +8,31 @@
 using namespace std;
 
 // --- LeetCode Solution ---
-class Solution {
+class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int n=matrix.size();
+        int m=matrix[0].size();
 
-        int low=0;
-        int high=m*n-1;
+        int low=0;
+        int high=m*n-1;
 
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int row=mid/m;
-            int col=mid%m;
-            if(matrix[row][col]==target) return true;
-            else if(matrix[row][col]<target){
-                low=mid+1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
 
 int main() {
     return 0;
