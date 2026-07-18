534f5c977a34d3419e6df039faac1f393670fca2
Jenil Mistry <jenil140509@gmail.com>
2026-07-18 20:00:11 +0530

    Update Insert_Interval solution

--- Insert_Interval.cpp
+++ Insert_Interval.cpp
@@ -8,12 +8,15 @@
 using namespace std;
 
 // --- LeetCode Solution ---
+// brute force would be to insert interval at end ans then again sort and use merge intervals algo
+//but our problem ha already sorted so we can optimise using that 
+
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
         int n=intervals.size();
-        
+
         int i=0;
         while(i<n && intervals[i][1]<newInterval[0]){
             ans.push_back(intervals[i]);
