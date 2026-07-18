5dd47a29b8043515d3bae69e0d965ca0727d50b4
Jenil Mistry <jenil140509@gmail.com>
2026-07-18 19:51:56 +0530

    Add Insert_Interval solution

---
+++ Insert_Interval.cpp
@@ -0,0 +1,40 @@
+/**
+ * LeetCode Problem: Insert Interval
+ * Pushed by LeetCommit
+ * Date: 2026-07-18
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int n=intervals.size();
+        
+        int i=0;
+        while(i<n && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+
+        while(i<n ){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
