282ae905e4df1511701828db8bfc9b3e5be24099
Jenil Mistry <jenil140509@gmail.com>
2026-07-19 16:45:53 +0530

    Add Remove_Duplicate_Letters.cpp solution

---
+++ Remove_Duplicate_Letters.cpp
@@ -0,0 +1,45 @@
+/**
+ * LeetCode Problem: Remove Duplicate Letters
+ * Pushed by LeetCommit
+ * Date: 2026-07-19
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+       int n=s.size();
+        string ans="";
+
+        vector<bool> taken(26,false);
+        vector<int> lastIdx(26,-1);
+        
+        for(int i=0;i<n;i++){
+            lastIdx[s[i]-'a']=i;
+        }
+        for(int i=0;i<n;i++){
+            char c=s[i];
+            int idx=c-'a';
+
+            if(taken[idx]==true) continue;
+
+            while( ans.size()>0 && ans.back()>c && lastIdx[ans.back()-'a'] > i ){
+                taken[ans.back()-'a']=false;
+                ans.pop_back();
+
+            }
+            ans.push_back(c);
+            taken[idx]=true;
+        }
+
+        return ans;
+ 
+    }
+};
+
+int main() {
+    return 0;
+}
