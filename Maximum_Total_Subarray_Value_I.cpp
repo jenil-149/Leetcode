2efe5b28ac74985c5caf71a061c76e6889d69efd
Jenil Mistry <jenil140509@gmail.com>
2026-07-20 16:39:09 +0530

    Add Maximum_Total_Subarray_Value_I.cpp solution

---
+++ Maximum_Total_Subarray_Value_I.cpp
@@ -0,0 +1,23 @@
+/**
+ * LeetCode Problem: Maximum Total Subarray Value I
+ * Pushed by LeetCommit
+ * Date: 2026-07-20
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int mx=*max_element(nums.begin(),nums.end());
+        int mn=*min_element(nums.begin(),nums.end());
+
+        return 1LL*k*(1LL*mx-1LL*mn);
+    }
+};
+
+int main() {
+    return 0;
+}
