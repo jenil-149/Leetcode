ec17d57cfdca11ea23684074489e4d641f002ab4
Jenil Mistry <jenil140509@gmail.com>
2026-07-22 23:49:58 +0530

    Update House_Robber_II.cpp solution

--- House_Robber_II.cpp
+++ House_Robber_II.cpp
@@ -10,42 +10,27 @@ using namespace std;
 // --- LeetCode Solution ---
 class Solution {
 public:
-    int rob1(vector<int>& nums) {
-        
-        int n=nums.size();
-        int prev1=nums[0];
-        int prev2=0;
-    
-        for(int i=1;i<n;i++){
-            int curr=0;
+    int rob1(vector<int>& nums, int l, int r) {
+        int prev1 = 0, prev2 = 0;
 
-            int notTake=prev1;
-            int take=nums[i];
-            if(i>1)  take+=prev2;
+        for (int i = l; i <= r; i++) {
+            int take = nums[i] + prev2;
+            int notTake = prev1;
 
-            curr=max(take,notTake);
+            int curr = max(take, notTake);
 
-            prev2=prev1;
-            prev1=curr;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
         return prev1;
     }
 
     int rob(vector<int>& nums) {
-        int n=nums.size();
-       vector<int> temp1,temp2;
-       for(int i=0;i<n-1;i++){
-        temp1.push_back(nums[i]);
-       }
-       for(int i=1;i<n;i++){
-        temp2.push_back(nums[i]);
-       }
+        int n = nums.size();
+        if (n == 1)return nums[0];
 
-       int ans1=rob1(temp1);
-       int ans2=rob1(temp2);
-
-       return max(ans1,ans2);
+        return max(rob1(nums, 0, n - 2), rob1(nums, 1, n - 1));
     }
 };
 
