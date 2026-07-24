25b1c279fe71443ad7cab8f6c98e79bd43d35d02
Jenil Mistry <jenil140509@gmail.com>
2026-07-24 22:42:19 +0530

    Add Bulb_Switcher.cpp solution

---
+++ Bulb_Switcher.cpp
@@ -0,0 +1,21 @@
+/**
+ * LeetCode Problem: Bulb Switcher
+ * Pushed by LeetCommit
+ * Date: 2026-07-24
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        //after some dry runs we get the numbers that are perfect squares have switvh on
+        return sqrt(n);
+    }
+};
+
+int main() {
+    return 0;
+}
