b9d454fceb52e697a3b8dbb469baf3127d4c1a2e
Jenil Mistry <jenil140509@gmail.com>
2026-08-06 23:40:37 +0530

    Add Flip_Columns_For_Maximum_Number_of_Equal_Rows.cpp solution

---
+++ Flip_Columns_For_Maximum_Number_of_Equal_Rows.cpp
@@ -0,0 +1,34 @@
+/**
+ * LeetCode Problem: Flip Columns For Maximum Number of Equal Rows
+ * Pushed by LeetCommit
+ * Date: 2026-08-06
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        unordered_map<string,int> mp;
+
+        for(auto x:matrix){
+            string s="";
+            for(auto y:x){
+                if(y==x[0]) s+="1";
+                else s+="0";
+            }
+            mp[s]++;
+        }
+        int ans=0;
+        for(auto &[s,val]:mp){
+            ans=max(ans,val);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
