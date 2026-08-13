d6370b6178884ceb5bec7532d77ce952ae549353
Jenil Mistry <jenil140509@gmail.com>
2026-08-13 10:51:52 +0530

    Add Maximum_Number_of_Occurrences_of_a_Substring.cpp solution

---
+++ Maximum_Number_of_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,35 @@
+/**
+ * LeetCode Problem: Maximum Number of Occurrences of a Substring
+ * Pushed by LeetCommit
+ * Date: 2026-08-13
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
+        unordered_map<string,int> mp;
+        
+        for(int i=0;i<=s.size()-minSize;i++){
+            unordered_set<char> cs={};
+            string temp="";
+            for(int j=i;j<(i+minSize);j++){
+                cs.insert(s[j]);
+                temp+=s[j];
+            }
+            if(cs.size()<=maxLetters) mp[temp]++;
+        }
+        int ans=0;
+        for(auto &[str,freq]:mp){
+            ans=max(ans,freq);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
