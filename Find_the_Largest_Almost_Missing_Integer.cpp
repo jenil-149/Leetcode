b270d5a9fed17aca0e732a98151df1771d9776a9
Jenil Mistry <jenil140509@gmail.com>
2026-08-18 11:34:04 +0530

    Add Find_the_Largest_Almost_Missing_Integer.cpp solution

---
+++ Find_the_Largest_Almost_Missing_Integer.cpp
@@ -0,0 +1,37 @@
+/**
+ * LeetCode Problem: Find the Largest Almost Missing Integer
+ * Pushed by LeetCommit
+ * Date: 2026-08-18
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> freq(51,0);
+
+        for(int n:nums) freq[n]++;
+        int mx=*max_element(nums.begin(),nums.end());
+        if(n==k){
+            return mx; 
+        }
+        if(k==1){
+            for(int i=50;i>=0;i--) {
+                if(freq[i]==1) return i;
+            }
+        }
+        int ans=-1;
+        if(freq[nums[0]]==1) ans=max(ans,nums[0]);
+        if(freq[nums[n-1]]==1) ans=max(ans,nums[n-1]);
+
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
