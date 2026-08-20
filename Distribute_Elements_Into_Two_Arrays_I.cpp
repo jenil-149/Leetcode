b379b1d32a6c8843905876fff5750a4aff483d2a
Jenil Mistry <jenil140509@gmail.com>
2026-08-20 10:29:10 +0530

    Add Distribute_Elements_Into_Two_Arrays_I.cpp solution

---
+++ Distribute_Elements_Into_Two_Arrays_I.cpp
@@ -0,0 +1,39 @@
+/**
+ * LeetCode Problem: Distribute Elements Into Two Arrays I
+ * Pushed by LeetCommit
+ * Date: 2026-08-20
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<int> arr1;
+        vector<int> arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        
+        for(int i=2;i<n;i++){
+            if(arr1.back()> arr2.back()){
+                arr1.push_back(nums[i]);
+            }else{
+                arr2.push_back(nums[i]);
+            }
+        }
+
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+
+        return arr1;
+
+
+    }
+};
+
+int main() {
+    return 0;
+}
