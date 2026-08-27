cc6dd96908906fe0af09a8bf61f8389cf38d0d98
Jenil Mistry <jenil140509@gmail.com>
2026-08-28 00:03:11 +0530

    Add Lexicographically_Smallest_Permutation_Greater_Than_Target.cpp solution

---
+++ Lexicographically_Smallest_Permutation_Greater_Than_Target.cpp
@@ -0,0 +1,64 @@
+/**
+ * LeetCode Problem: Lexicographically Smallest Permutation Greater Than Target
+ * Pushed by LeetCommit
+ * Date: 2026-08-27
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    string result = "";
+
+    bool solve(string& curr, vector<int>& count, string& target, int i, bool greater) {
+        if(i == target.length()) {
+            if(greater) {
+                result = curr;
+                return true;
+            }
+            return false;
+        }
+
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            if(count[ch-'a'] == 0)
+                continue;
+            
+            if(greater == false && ch < target[i])
+                continue;
+            
+
+            curr.push_back(ch);
+            count[ch-'a']--;
+
+            bool isGreater = greater || ch > target[i];
+
+            if(solve(curr, count, target, i+1, isGreater)) {
+                return true;
+            }
+
+            curr.pop_back();
+            count[ch-'a']++;
+        }
+
+        return false;
+    }
+
+    string lexGreaterPermutation(string s, string target) {
+        vector<int> count(26, 0);
+
+        for(char &ch : s)
+            count[ch-'a']++;
+        
+        string curr;
+
+        solve(curr, count, target, 0, false);
+
+        return result;
+    }
+};
+
+int main() {
+    return 0;
+}
