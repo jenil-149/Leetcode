7cb59a2156f472fe5925c2607b3c4268b2764968
Jenil Mistry <jenil140509@gmail.com>
2026-08-28 13:56:33 +0530

    Update Divide_Array_Into_Arrays_With_Max_Difference.cpp solution

--- Divide_Array_Into_Arrays_With_Max_Difference.cpp
+++ Divide_Array_Into_Arrays_With_Max_Difference.cpp
@@ -17,7 +17,7 @@ public:
         for(int i=0;i<=n-3;i+=3){
             ans.push_back({nums[i],nums[i+1],nums[i+2]});
 
-            if(nums[i+2]-nums[i+1]>k || nums[i+1]-nums[i]>k || nums[i+2]-nums[i]>k) return {};
+            if(nums[i+2]-nums[i]>k) return {};
         }
         return ans;
     }
