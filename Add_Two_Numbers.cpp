42131ff437c4cc643621b2b424d3b9475f59b28c
Jenil Mistry <jenil140509@gmail.com>
2026-09-02 19:08:57 +0530

    Update Add_Two_Numbers.cpp solution

--- Add_Two_Numbers.cpp
+++ Add_Two_Numbers.cpp
@@ -1,23 +1,14 @@
 /**
  * LeetCode Problem: Add Two Numbers
  * Pushed by LeetCommit
- * Date: 2026-07-13
+ * Date: 2026-09-02
  */
 
 #include <bits/stdc++.h>
 using namespace std;
 
 // --- LeetCode Solution ---
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
