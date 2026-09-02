34b20c82cb8e40d41997955b16b031367a52ff24
Jenil Mistry <jenil140509@gmail.com>
2026-09-02 19:30:36 +0530

    Update Number_of_Dice_Rolls_With_Target_Sum.cpp solution

--- Number_of_Dice_Rolls_With_Target_Sum.cpp
+++ Number_of_Dice_Rolls_With_Target_Sum.cpp
@@ -10,25 +10,25 @@ using namespace std;
 // --- LeetCode Solution ---
 class Solution {
 public:
-    int dp[31][31][1001];
+    int dp[31][1001];
     const int mod=1e9+7;
     int f(int n,int k,int t){
         
         if(n==0 && t==0) return 1;
         if(t<0 || n==0) return 0;
-        if(dp[n][k][t]!=-1) return dp[n][k][t];
+        if(dp[n][t]!=-1) return dp[n][t];
 
         long long ans=0;
         for(int i=1;i<=k;i++){
             ans=(ans+f(n-1,k,t-i)%mod);
         }
-        return dp[n][k][t]=(int)(ans%mod);
+        return dp[n][t]=(int)(ans%mod);
 
     }
     int numRollsToTarget(int n, int k, int target) {
         memset(dp,-1,sizeof(dp));
         f(n,k,target);
-        return dp[n][k][target];
+        return dp[n][target];
     }
 };
 
