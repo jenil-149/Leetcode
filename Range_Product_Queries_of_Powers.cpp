37f047dee3bd1ced55a1e95ce3cd487d80dc4716
Jenil Mistry <jenil140509@gmail.com>
2026-09-09 21:47:16 +0530

    Update Range_Product_Queries_of_Powers.cpp solution

--- Range_Product_Queries_of_Powers.cpp
+++ Range_Product_Queries_of_Powers.cpp
@@ -10,38 +10,51 @@ using namespace std;
 // --- LeetCode Solution ---
 class Solution {
 public:
-    typedef long long ll;
-    const int mod = 1e9 + 7;
+    using ll = long long;
+
+    const ll MOD = 1e9 + 7;
+
+    ll binpow(ll base, int exp) {
+        ll ans = 1;
+
+        while (exp > 0) {
+            if (exp & 1)
+                ans = ans * base % MOD;
+
+            base = base * base % MOD;
+            exp >>= 1;
+        }
+
+        return ans;
+    }
 
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
 
+        // Store exponents of set bits
         vector<int> powers;
 
         for (int k = 0; (1LL << k) <= n; k++) {
-            if (n & (1LL << k)) {
+            if (n & (1LL << k))
                 powers.push_back(k);
-            }
         }
 
+        // Prefix sum of exponents
         vector<int> prefix(powers.size() + 1, 0);
 
-        for (int i = 1; i <= powers.size(); i++) {
-            prefix[i] = prefix[i - 1] + powers[i - 1];
+        for (int i = 0; i < powers.size(); i++) {
+            prefix[i + 1] = prefix[i] + powers[i];
         }
 
         vector<int> ans;
-        for (auto q : queries) {
+
+        for (auto &q : queries) {
             int l = q[0];
             int r = q[1];
 
-            int p = prefix[r + 1] - prefix[l];
-            ll a = 1;
+            // Sum of powers from l to r
+            int exponent = prefix[r + 1] - prefix[l];
 
-            for (int i = 0; i < p; i++) {
-                a = (a * 2) % mod;
-            }
-
-            ans.push_back((int)a);
+            ans.push_back(binpow(2, exponent));
         }
 
         return ans;
