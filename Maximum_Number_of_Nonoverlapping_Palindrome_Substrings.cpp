83241bd1573fbdec1fa3423a87df14c255f9b5f4
Jenil Mistry <jenil140509@gmail.com>
2026-09-15 23:11:53 +0530

    Add Maximum_Number_of_Nonoverlapping_Palindrome_Substrings.cpp solution

---
+++ Maximum_Number_of_Nonoverlapping_Palindrome_Substrings.cpp
@@ -0,0 +1,60 @@
+/**
+ * LeetCode Problem: Maximum Number of Non-overlapping Palindrome Substrings
+ * Pushed by LeetCommit
+ * Date: 2026-09-15
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- LeetCode Solution ---
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+    vector<int> t;
+
+    int solve(int n, int k) {
+        if (n < k) return 0;
+
+        if (t[n] != -1) 
+            return t[n];
+
+        int result = solve(n - 1, k); //ignore the current character s[n-1]
+
+        int j = n - 1;
+        //end the current palindrome at the current character s[n-1]
+        for (int i = 0; j-i+1>=k; i++) {
+            if (isPalindrome[i][j]) {
+                result = max(result, 1 + solve(i, k));
+            }
+        }
+
+        return t[n] = result;
+    }
+
+    int maxPalindromes(string s, int k) {
+        int n = s.length();
+        isPalindrome.assign(n, vector<bool>(n, false));
+
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
+                int j = i + L - 1;
+
+                if (i == j) {
+                    isPalindrome[i][i] = true;
+                } else if (i + 1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]);
+                } else {
+                    isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+                }
+            }
+        }
+
+        t.assign(n + 1, -1);
+        return solve(n, k);
+    }
+};
+
+int main() {
+    return 0;
+}
