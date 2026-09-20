4f4a882c74ce658716d9d3cb5c39ea18ee67685a
Jenil Mistry <jenil140509@gmail.com>
2026-09-20 13:14:28 +0530

    Update Find_All_Groups_of_Farmland.cpp solution

--- Find_All_Groups_of_Farmland.cpp
+++ Find_All_Groups_of_Farmland.cpp
@@ -10,6 +10,72 @@ using namespace std;
 // --- LeetCode Solution ---
 class Solution {
 public:
+    int n, m;
+
+    int dr[4] = {-1, 0, 1, 0};
+    int dc[4] = {0, -1, 0, 1};
+
+    bool isValid(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        n = land.size();
+        m = land[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (land[i][j] == 1 && !vis[i][j]) {
+
+                    int x1 = i;
+                    int y1 = j;
+
+                    int x2 = i;
+                    int y2 = j;
+
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+
+                    while (!q.empty()) {
+
+                        auto [x, y] = q.front();
+                        q.pop();
+
+                        // Update bottom-right corner
+                        x2 = max(x2, x);
+                        y2 = max(y2, y);
+
+                        for (int k = 0; k < 4; k++) {
+
+                            int nx = x + dr[k];
+                            int ny = y + dc[k];
+
+                            if (isValid(nx, ny) &&
+                                land[nx][ny] == 1 &&
+                                !vis[nx][ny]) {
+
+                                vis[nx][ny] = 1;
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
+
+                    ans.push_back({x1, y1, x2, y2});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+class Solution {
+public:
     vector<vector<int>> vis;
     int n,m;
 
